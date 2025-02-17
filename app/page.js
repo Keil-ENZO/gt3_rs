ab9111a344d72aba75cb80a67eab3a46a23d2ae8
Keil-ENZO <enzo.keil06@icloud.com>
2025-02-17 19:33:55 +0100

    deploy vercel

--- app/page.js
+++ app/page.js
@@ -1,21 +1,162 @@
-"use client"; // Marque le composant comme étant rendu côté client
+"use client";
 
+import Lenis from "@studio-freight/lenis";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
-import ZoomImg from "../Components/ZoomImg";
+import { useEffect, useRef } from "react";
 import Logo from "../app/assets/logo.svg";
-
 import "./globals.css";
 
-export default function Home() {
+const GT3RSPage = () => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 3]);
+  const scale5 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+  const scale6 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  const scale8 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+  const scale9 = useTransform(scrollYProgress, [0, 1], [1, 7]);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div>
+      {/* Header Section */}
       <div className="p-20 m-20">
         <Image src={Logo} alt="img" className="w-full" />
         <p className="text-5xl">911 GT3 RS</p>
       </div>
 
-      <ZoomImg />
+      {/* Image Zoom Section */}
+      <div ref={container} className="h-[300vh] w-[100%] relative mb-[10vh]">
+        <div className="sticky top-0 h-[100vh] overflow-hidden">
+          <motion.div
+            style={{ scale: scale4 }}
+            className="w-[100%] h-[100%] absolute top-0 flex items-center justify-center"
+          >
+            <div className="w-[35vw] h-[25vw] relative">
+              <Image
+                src="/picture8.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
 
+          <motion.div
+            style={{ scale: scale5 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[25vw] h-[15vw] top-[20vh] right-[32vw] relative">
+              <Image
+                src="/picture2.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            style={{ scale: scale6 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[25vw] h-[15vw] top-[-35vh] right-[-35vw] relative">
+              <Image
+                src="/picture9.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            style={{ scale: scale6 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[25vw] h-[15vw] top-[-5vh] left-[30vw] relative">
+              <Image
+                src="/picture10.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            style={{ scale: scale8 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[25vw] h-[15vw] top-[-35vh] right-[35vw] relative">
+              <Image
+                src="/picture1.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            style={{ scale: scale9 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[28vw] h-[18vw] top-[35vh] right-[-5vw] relative">
+              <Image
+                src="/picture5.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            style={{ scale: scale6 }}
+            className="w-[100%] h-[100%] absolute top-0 m-5 flex items-center justify-center"
+          >
+            <div className="w-[28vw] h-[18vw] top-[35vh] right-[-35vw] relative">
+              <Image
+                src="/picture4.webp"
+                alt="img"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+                className="object-cover"
+              />
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* Stats Section */}
       <div className="flex flex-col justify-center items-center mb-[20px]">
         <div className="w-[50vw] flex justify-between items-center">
           <div>
@@ -43,4 +184,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default GT3RSPage;
