c23d2c865c9238baa17305aded7ba9dc8cbf1e74
Keil-ENZO <enzo.keil06@icloud.com>
2025-03-18 18:01:51 +0100

    SEO

--- app/layout.js
+++ app/layout.js
@@ -10,20 +10,42 @@ const inter = {
     }
   `,
 };
-<head>
-  <link rel="icon" href="./favicon.ico" />;
-</head>;
 
 export const metadata = {
-  title: "GT3 RS - 2023",
+  title: "Porsche GT3 RS - 2023 | Supercar de Performance Ultime",
   description:
-    "Project GT3 RS - 2023 with Next.js, Tailwind CSS, GSAP, and WEBGL",
+    "Découvrez la Porsche GT3 RS 2023, la supercar de course légale sur route avec une puissance extraordinaire, une aérodynamique avancée et une performance sans compromis.",
+  keywords:
+    "Porsche GT3 RS, GT3 RS 2023, supercar, voiture de course, Porsche de course, performance automobile",
+
+  formatDetection: {
+    email: false,
+    telephone: false,
+    address: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: [{ url: "/favicon.ico" }, { url: "/icon.png", type: "image/png" }],
+    apple: [{ url: "/apple-icon.png" }],
+  },
+  other: {
+    "color-scheme": "dark",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="${inter.className} overflow-x-hidden">{children}</body>
+    <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <style dangerouslySetInnerHTML={{ __html: inter.fontFace }} />
+        <link rel="canonical" href="https://gt3rs.com" />
+      </head>
+      <body className={inter.className + " overflow-x-hidden"}>{children}</body>
     </html>
   );
 }
